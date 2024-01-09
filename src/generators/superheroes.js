a2b5cf3b8589954a28d9e1a4c6e83f51fa1d70ca
Septillihedron <resha3adnandaru@gmail.com>
2024-01-09 11:15:26 +0700

    Clean up the compiler

--- src/generators/superheroes.js
+++ src/generators/superheroes.js
@@ -1,14 +1,32 @@
 import * as Blockly from 'blockly';
 
+const typeFieldKey = {
+	"triggers": "type",
+	"conditions": "type",
+	"effects": "type",
+	"skills": "skill",
+	"damagemodifiers": "type",
+	"rewards": "type",
+	"distributions": "type",
+}
+
 export const superheroesGenerator = new Blockly.Generator('Superheroes');
 
-superheroesGenerator['workspaceToCode'] = function(workspace) {
+/**
+ * @param {Blockly.Workspace | undefined} workspace 
+ * @returns {string}
+ */
+superheroesGenerator.workspaceToCode = function(workspace) {
+    if (!workspace) return ""
     return workspace.getTopBlocks(true)
         .map(block => superheroesGenerator.blockToCode(block))
         .join("\r\n\r\n");
 }
 
-superheroesGenerator['blockToCode'] = function(block) {
+/**
+ * @param {Blockly.Block | null} block 
+ */
+superheroesGenerator.blockToCode = function(block) {
     if (block == null) return '';
 	if (superheroesGenerator[block.type] !== undefined) return superheroesGenerator[block.type](block);
 	const [block_category, _] = block.type.split("_");
@@ -16,7 +34,7 @@ superheroesGenerator['blockToCode'] = function(block) {
     return blockToYaml(block);
 }
 
-superheroesGenerator['listtypes'] = function(block, generator) {
+superheroesGenerator['listtypes'] = function(block) {
 	const field = block.getFieldValue("VALUE")
 	if (field != null) return field
 	const input = getInputBlock(block, "VALUE")
@@ -29,54 +47,75 @@ superheroesGenerator['types_range'] = function(block) {
 	return `${min} - ${max}`
 }
 
+/**
+ * @param {Blockly.Block} block
+ * @param {string} name
+ */
 function getInputBlock(block, name) {
-	return block.getInput(name).connection.targetBlock()
+	return block.getInput(name)?.connection?.targetBlock()
 }
 
-const typeField = {
-	"triggers": "type",
-	"conditions": "type",
-	"effects": "type",
-	"skills": "skill",
-	"damagemodifiers": "type",
-	"rewards": "type",
-	"distributions": "type",
-}
-// default yaml serialization
+/**
+ * default yaml serialization
+ * @param {Blockly.Block} block
+ */
 function blockToYaml(block) {
-    const blockFields = getBlockFields(block);
-    const [block_category, block_name] = block.type.split("_");
-	if (block_category in typeField) {
-		blockFields.unshift([typeField[block_category], block_name])
-	}
-    let sectionName = getBlockSectionName(blockFields);
+    const blockFields = getBlockFields(block)
+    const sectionName = getBlockSectionName(blockFields)
     
-    const valueEntries = blockFields.map(keyval => `${keyval[0]}: ${keyval[1]}`);
-    const objectEntries = [
-		...getBlockValues(block).map(value => `${value[0]}: ${indent(superheroesGenerator.blockToCode(value[1]))}`),
-		...blockStatementsToYaml(getBlockStatements(block))
-    ];
+    const fieldEntries = blockFieldsToYaml(blockFields)
+    const valueEntries = blockValuesToYaml(getBlockValues(block))
+    const statementEntries = blockStatementsToYaml(getBlockStatements(block))
     
-	const entriesString = [...valueEntries, ...objectEntries].join("\r\n")
-    if (sectionName === undefined) return "\r\n" + entriesString;
-    return `\r\n${sectionName}: ` + indent("\r\n" + entriesString);
+	const entriesString = [...fieldEntries, ...valueEntries, ...statementEntries].join("\r\n")
+    if (sectionName === undefined) {
+        return "\r\n" + entriesString
+    } else {
+        return `\r\n${sectionName}: ` + indent("\r\n" + entriesString)
+    }
 }
 
+/**
+ * @param {{key: string, value: any}[]} blockFields
+ */
 function getBlockSectionName(blockFields) {
-    const sectionName = blockFields.find(field => field[0] == "SECTION_NAME");
-    if (sectionName === undefined) return undefined;
-    const sectionNameIndex = blockFields.indexOf(sectionName);
+    const sectionNameIndex = blockFields.findIndex(field => field.key == "SECTION_NAME");
+    if (sectionNameIndex === -1) return undefined;
+	const sectionName = blockFields[sectionNameIndex].value
     blockFields.splice(sectionNameIndex, 1);
-    return sectionName[1];
+    return sectionName;
 }
 
+/**
+ * @param {{ key: string, value: any }[]} fields
+ */
+function blockFieldsToYaml(fields) {
+    return fields.map(({key, value}) => {
+        return `${key}: ${value}`
+    })
+}
+
+/**
+ * @param {{ key: string, value: Blockly.Block }[]} values
+ */
+function blockValuesToYaml(values) {
+    return values.map(({key, value}) => {
+        const valueStr = indent(superheroesGenerator.blockToCode(value))
+        return `${key}: ${valueStr}`
+    })
+}
+
+/**
+ * @param {{ key: string, value: Blockly.Block[] }[]} statements
+ */
 function blockStatementsToYaml(statements) {
     return statements.map(statement => {
-            let [statementName, statementList] = statement
-			const isList = statementName.endsWith("_array")
-			statementName = statementName.substring(0, statementName.length - (isList? "_array".length : 0))
+            let statementKey = statement.key
+            const blockList = statement.value
+			const isList = statementKey.endsWith("_array")
+			statementKey = statementKey.substring(0, statementKey.length - (isList? "_array".length : 0))
 			
-			let statementListString = statementList
+			let blockListString = blockList
 				.map(block => superheroesGenerator.blockToCode(block))
 				.map(block => indent(block))
 				.map((block, index) => {
@@ -84,48 +123,103 @@ function blockStatementsToYaml(statements) {
 						block = block.trimStart()
 						return `\r\n- ${block}`
 					} else {
-						const key = statementName + index
+						const key = statementKey + index
 						return `\r\n${key}: ${block}`
 					}
 				})
 				.join("")
+            blockListString = indent(blockListString)
 			
-            return `${statementName}: ${indent(statementListString)}`
+            return `${statementKey}: ${blockListString}`
         })
 }
 
+/**
+ * @param {Blockly.Block} block 
+ * @returns {{key: string, value: any}[]}
+ */
 function getBlockFields(block) {
-    return block.inputList
+    const blockFields = block.inputList
         .flatMap(input => input.fieldRow)
         .filter(field => !isFieldLabel(field))
         .map(field => field.name)
-        .map(fieldName => [fieldName, block.getFieldValue(fieldName)])
-		.filter(x => x[1])
+        .filter(notUndefined)
+        .map(fieldName => definedEntry(fieldName, block.getFieldValue(fieldName)))
+		.filter(notUndefined)
+    
+    // adds in the type field
+    const [block_category, block_name] = block.type.split("_");
+	if (block_category in typeFieldKey) {
+		blockFields.unshift(entry(typeFieldKey[block_category], block_name))
+	}
+
+    return blockFields
 }
 
+/**
+ * @template {any} T
+ * @param {T | undefined} x
+ * @returns {x is T}
+ */
+function notUndefined(x) {
+    return x !== undefined
+}
+
+/**
+ * @template K, V
+ * @param {NonNullable<K>} key
+ * @param {NonNullable<V>} value
+ */
+function entry(key, value) {
+    return {key, value}
+}
+
+/**
+ * @template K, V
+ * @param {K} key
+ * @param {V} value
+ */
+function definedEntry(key, value) {
+    if (key == undefined) return undefined
+    if (value == undefined) return undefined
+    return entry(key, value)
+}
+
+/**
+ * @param {Blockly.Block} block
+ * @returns {{ key: string, value: Blockly.Block }[]}
+ */
 function getBlockValues(block) {
     return block.inputList
         .filter(input => getInputType(input) === 'value')
-        .map(input => [input.name, input.connection.targetBlock()])
-        .filter(statement => statement[1] !== null);
+        .map(input => definedEntry(input.name, input.connection?.targetBlock()))
+        .filter(notUndefined);
 }
 
+/**
+ * @param {Blockly.Block} block
+ * @returns {{ key: string, value: Blockly.Block[] }[]}
+ */
 function getBlockStatements(block) {
     return block.inputList
         .filter(input => getInputType(input) === 'statement')
-        .map(input => [input.name, [input.connection.targetBlock()]])
-        .filter(statement => statement[1][0] !== null)
+        .map(input => definedEntry(input.name, input.connection?.targetBlock()))
+        .filter(notUndefined)
         .map(statement => {
-            let [_, statementList] = statement;
-            let currentBlock = statementList[0];
-            if (currentBlock == undefined) return;
-            while ((currentBlock = currentBlock.getNextBlock()) !== null) {
-                statementList.push(currentBlock);
+            const {key, value} = statement;
+            const blockList = [value]
+            /** @type {Blockly.Block | null} */
+            let currentBlock = value
+            while ((currentBlock = currentBlock?.getNextBlock()) !== null) {
+                blockList.push(currentBlock);
             }
-            return statement;
+            return entry(key, blockList);
         });
 }
 
+/**
+ * @param {Blockly.Input} input
+ */
 function getInputType(input) {
     const type = input.type;
     if (type == 1) return 'value';
@@ -133,10 +227,20 @@ function getInputType(input) {
     if (type == 5) return 'dummy';
 }
 
+/**
+ * @template {Blockly.Field} T
+ * @param {T} field 
+ * @returns {boolean}
+ */
 function isFieldLabel(field) {
     return field instanceof Blockly.FieldLabel
 }
 
+// [string, number] type is from the quirk of generator's blockToCode
+/**
+ * @param {string | [string, number]} str
+ */
 function indent(str, indent = 2, character = " ") {
-    return str.replace(/\r\n/g, "\r\n" + character.repeat(indent));
+    if (typeof str !== "string") str = str[0]
+    return str.replace(/\r\n/g, "\r\n" + character.repeat(indent))
 }
